12bf0805e4234ec80b7f69455b2ca9508fe06438
Prabhat Kumar <4979@users.noreply.github.com>
2015-07-09 17:48:06 +0530

    update of run_analysis.R - commit.
    Step-01 : Merges the training and the test sets to create one data set.

--- run_analysis.R
+++ run_analysis.R
@@ -13,3 +13,24 @@
 
 # Note: To Set the Working Directory,
 # setwd("~/Desktop/Coursera/Getting-and-Cleaning-Data/Course-Project/")
+
+# Step-01 : After reading training and test data set,
+#           Merges the training and the test sets to create one data set.
+
+trainData <- read.table("./data/train/X_train.txt")
+dim(trainData)                                                # 7352*561
+head(trainData)
+trainLabel <- read.table("./data/train/y_train.txt")
+table(trainLabel)
+trainSubject <- read.table("./data/train/subject_train.txt")
+testData <- read.table("./data/test/X_test.txt")
+dim(testData)                                                 # 2947*561
+testLabel <- read.table("./data/test/y_test.txt") 
+table(testLabel) 
+testSubject <- read.table("./data/test/subject_test.txt")
+joinData <- rbind(trainData, testData)
+dim(joinData)                                                 # 10299*561
+joinLabel <- rbind(trainLabel, testLabel)
+dim(joinLabel)                                                # 10299*1
+joinSubject <- rbind(trainSubject, testSubject)
+dim(joinSubject)                                              # 10299*1
