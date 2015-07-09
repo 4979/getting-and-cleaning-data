fbccc1a4fa80c7b2104f6406bc24ce8acafd70e2
Prabhat Kumar <4979@users.noreply.github.com>
2015-07-09 17:35:47 +0530

    update of run_analysis.R - commit.

--- run_analysis.R
+++ run_analysis.R
@@ -2,3 +2,11 @@
 # @author : Prabhat Kumar
 # @date   : 09-July-2015
 # =========================
+
+# Instructions:
+
+# Step-01 : Merges the training and the test sets to create one data set.
+# Step-02 : Extracts only the measurements on the mean and standard deviation for each measurement.
+# Step-03 : Uses descriptive activity names to name the activities in the data set.
+# Step-04 : Appropriately labels the data set with descriptive variable names.
+# Step-05 : From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
