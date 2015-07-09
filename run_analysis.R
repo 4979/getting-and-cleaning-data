7c6b41c082ab0d81f94f1eb7742e3733a8fff472
Prabhat Kumar <4979@users.noreply.github.com>
2015-07-09 17:40:03 +0530

    update of run_analysis.R - commit.

--- run_analysis.R
+++ run_analysis.R
@@ -10,3 +10,6 @@
 # Step-03 : Uses descriptive activity names to name the activities in the data set.
 # Step-04 : Appropriately labels the data set with descriptive variable names.
 # Step-05 : From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+
+# Note: To Set the Working Directory,
+# setwd("~/Desktop/Coursera/Getting-and-Cleaning-Data/Course-Project/")
